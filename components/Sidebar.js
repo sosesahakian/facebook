b12cdcfcae7c3674c006d28d0d3e163cabe50e68
Sose Sahakian <sose94@gmail.com>
2022-06-04 12:33:26 +0200

    initial

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import{
-    ChevronDownIcon,
-    ShoppingBagIcon,
-    UserGroupIcon,
-} from '@heroicons/react/outLine';
-import { ChevronUpIcon } from '@heroicons/react/outLine';
-import {
-    CalendarIcon,
-    ClockIcon,
-    DesktopComputerIcon,
-    UsersIcon,
-} from '@heroicons/react/solid';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import DesktopWindowsIcon from '@mui/icons-material/DesktopWindows';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+
 import SidebarRow from './SidebarRow';
 
 function Sidebar() {
   return (
     <div className='p-2 mt-5 max-w-[600px] xl:min-w[300px]'>
-        <SidebarRow Icon={UsersIcon} title='Friends'/>
-        <SidebarRow Icon={UserGroupIcon} title='Groups'/>
+        <SidebarRow Icon={PeopleAltIcon} title='Friends'/>
+        <SidebarRow Icon={PeopleOutlineIcon} title='Groups'/>
         <SidebarRow Icon={ShoppingBagIcon} title='Marketplace'/>
-        <SidebarRow Icon={DesktopComputerIcon} title='Watch'/>
-        <SidebarRow Icon={CalendarIcon} title='Events'/>
-        <SidebarRow Icon={ClockIcon} title='Memories'/>
-        <SidebarRow Icon={ChevronDownIcon} title='See More'/>
+        <SidebarRow Icon={DesktopWindowsIcon} title='Watch'/>
+        <SidebarRow Icon={CalendarMonthIcon} title='Events'/>
+        <SidebarRow Icon={AccessTimeIcon} title='Memories'/>
+        <SidebarRow Icon={ExpandMoreIcon} title='See More'/>
 
     </div>
   )
